1eb8264da7df144a0d1de4ca3fd82e95ae9af944
SaltyUA <104589666+SaltyUA@users.noreply.github.com>
2023-12-16 14:17:43 +0200

    new schemas

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,4 +1,28 @@
 const Joi = require("joi");
+const { Schema, model } = require("mongoose");
+const handleMongooseError = require("../helpers/handleMongooseError");
+
+const ContactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false }
+);
+
+ContactSchema.post("save", handleMongooseError);
 
 const addSchema = Joi.object({
   name: Joi.string().required(),
@@ -12,7 +36,19 @@ const putSchema = Joi.object({
   phone: Joi.string(),
 }).or("name", "email", "phone");
 
-module.exports = {
+const patchSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const Contact = model("Contact", ContactSchema);
+
+const schemas = {
   addSchema,
   putSchema,
+  patchSchema,
+};
+
+module.exports = {
+  schemas,
+  Contact,
 };
