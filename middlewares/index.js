64fe1eaa1d2d80df1c6217869f7fe3bd272c8c6f
Vlad <00cmb.ua@gmail.com>
2024-01-03 12:18:03 +0200

    upload new avatar now true

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
-const bodyValidate = require("./bodyValidate");
-const isValidId = require("./isValidId");
-const tokenValidation = require("./tokenValidation");
+const bodyValidate = require('./bodyValidate');
+const isValidId = require('./isValidId');
+const tokenValidation = require('./tokenValidation');
+const upload = require('./upload');
 
 module.exports = {
   bodyValidate,
   isValidId,
   tokenValidation,
+  upload,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,22 +1,29 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { bodyValidate, tokenValidation } = require("../../middlewares");
-const { authCtrl } = require("../../controllers");
-const { schemas } = require("../../schemas/user");
+const { bodyValidate, tokenValidation, upload } = require('../../middlewares');
+const { authCtrl } = require('../../controllers');
+const { schemas } = require('../../schemas/user');
 
-router.post("/register", bodyValidate(schemas.userSchema), authCtrl.register);
+router.post('/register', bodyValidate(schemas.userSchema), authCtrl.register);
 
-router.post("/login", bodyValidate(schemas.userSchema), authCtrl.login);
+router.post('/login', bodyValidate(schemas.userSchema), authCtrl.login);
 
-router.post("/logout", tokenValidation, authCtrl.logout);
+router.post('/logout', tokenValidation, authCtrl.logout);
 
-router.get("/current", tokenValidation, authCtrl.currentUser);
+router.get('/current', tokenValidation, authCtrl.currentUser);
 
 router.patch(
-  "/",
+  '/',
   tokenValidation,
   bodyValidate(schemas.patchSubscription),
   authCtrl.patchSubscription
+);
+
+router.patch(
+  '/avatars',
+  tokenValidation,
+  upload.single('avatar'),
+  authCtrl.updateAvatar
 );
 
 module.exports = router;
