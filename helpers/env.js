bcffdbbcc928af4003de828c4c83888f3e228402
Vlad <00cmb.ua@gmail.com>
2024-01-07 20:12:18 +0200

    nodemailer setup

--- helpers/env.js
+++ helpers/env.js
@@ -1,20 +1,32 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
 dotenv.config();
 
-const { DB_MONGO_HOST, JWT_SECRET } = process.env;
+const { DB_MONGO_HOST, JWT_SECRET, EMAIL_PASSWORD, BASE_URL } = process.env;
 
 if (!DB_MONGO_HOST) {
-  console.log("DB_MONGO_HOST is not set");
+  console.log('DB_MONGO_HOST is not set');
   process.exit(1);
 }
 
 if (!JWT_SECRET) {
-  console.log("DB_MONGO_DATABASE is not set");
+  console.log('DB_MONGO_DATABASE is not set');
+  process.exit(1);
+}
+
+if (!EMAIL_PASSWORD) {
+  console.log('DB_MONGO_DATABASE is not set');
+  process.exit(1);
+}
+
+if (!BASE_URL) {
+  console.log('DB_MONGO_DATABASE is not set');
   process.exit(1);
 }
 
 module.exports = {
   JWT_SECRET,
   DB_MONGO_HOST,
+  EMAIL_PASSWORD,
+  BASE_URL,
 };
